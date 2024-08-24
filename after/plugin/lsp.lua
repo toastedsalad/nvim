5c3e8a6a458b1df6b825a11bf7aa8fa4c732c378
toastedsalad <blazys@pm.me>
2024-08-24 17:18:50 +0300

    Adding new debugger remaps cause we got the F10 key working.

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -5,12 +5,12 @@ local lsp_attach = function(client, bufnr)
   local opts = {buffer = bufnr}
 
   vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
-  vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
-  vim.keymap.set('n', '<F1>', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
+  vim.keymap.set('n', '<F5>', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
+  vim.keymap.set('n', '<F4>', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
   vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
-  vim.keymap.set('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
-  vim.keymap.set({'n', 'x'}, '<F3>', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
-  vim.keymap.set('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
+  vim.keymap.set('n', '<F3>', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
+  vim.keymap.set({'n', 'x'}, '<F2>', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
+  vim.keymap.set('n', '<F1>', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
 end
 
 lsp_zero.extend_lspconfig({

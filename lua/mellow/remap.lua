6235c78092a8330c0c41379949410d49227f8590
Gediminas Blazys <blazys@pm.me>
2024-09-22 21:25:42 +0300

    Adding the rest of the keymaps.

--- lua/mellow/remap.lua
+++ lua/mellow/remap.lua
@@ -19,19 +19,30 @@ vim.keymap.set('n', '<Leader>dl', dap.run_last, { silent = true })
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
-vim.keymap.set('v', '<leader>y', '"+y', { noremap = true, silent = true })
+vim.keymap.set("n", "J", "mzJ`z")
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "nzzzv")
 
+vim.keymap.set("x", "p", "\"_dP")
 
+vim.keymap.set('n', '<leader>y', "\"+y")
+vim.keymap.set('v', '<leader>y', "\"+y")
+vim.keymap.set('n', '<leader>Y', "\"+Y")
 
+vim.keymap.set('n', 'd', "\"_d")
+vim.keymap.set('v', 'd', "\"_d")
 
+vim.keymap.set("n", "Q", "<nop>")
+-- vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+vim.keymap.set("n", "<Leader>s", [[:%s/<C-r><C-w>//g<Left><Left>]])
+-- Not sure what this is doing, I probably need to lear about the sessionizer.
+-- vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
 
+-- This is for the quickfix list navigation but I don't know what that even is...
 
-
-
-
-
-
-
-
-
-
+-- vim.keymap.set('v', '<C-k>', "<cmd>cnext<CR>zz")
+-- vim.keymap.set('v', '<C-j>', "<cmd>cprev<CR>zz")
+-- vim.keymap.set('v', '<leader>k', "<cmd>lnext<CR>zz")
+-- vim.keymap.set('v', '<leader>j', "<cmd>lprev<CR>zz")

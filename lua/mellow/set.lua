5dca1750c652337cf2a0dd478d4fb627b16ae4b2
Gediminas Blazys <blazys@pm.me>
2025-11-09 12:06:02 +0200

    More changes

--- lua/mellow/set.lua
+++ lua/mellow/set.lua
@@ -6,6 +6,17 @@ vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 
+-- Override for specific filetypes
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "javascript", "typescript", "javascriptreact", "typescriptreact", "json", "jsonc" },
+  callback = function()
+    vim.opt_local.tabstop = 2
+    vim.opt_local.softtabstop = 2
+    vim.opt_local.shiftwidth = 2
+    vim.opt_local.expandtab = true
+  end,
+})
+
 vim.opt.exrc = true
 vim.opt.secure = true
 
